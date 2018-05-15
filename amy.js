bfb7ef604a3d09aae8808a85be3066c425fcd35d
Amy <lin53mp6@gmail.com>
2018-05-15 21:17:42 +0800

    nothing

---
+++ amy.js
@@ -0,0 +1,9 @@
+
+console.log('string');
+console.count('label');
+console.count('label');
+console.countReset('label');
+console.count('label');
+
+console.log(__filename);
+console.log(process.env.NODE_ENV);
