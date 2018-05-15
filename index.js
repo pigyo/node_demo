d1873f8f5570aefd3dfefd6a87d4c5a82d05deed
Amy <lin53mp6@gmail.com>
2018-05-15 20:38:05 +0800

    helmet & serve-static & 顯示htt[://localhost:3000/image.jpg

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
 const Express = require('express');
-
+const helmet =  require('helmet');
+const serveStatic = require('serve-static');
+// const publicPath = path.join
 const app = Express();
 
+app.use(helmet());
+app.use(serveStatic('public'));
+
+app.use((req, res, next) => {
+    console.log('middleware');
+    next();    
+})
+
 app.get('/',function(req, res){
-    console.log('hello');
-    console.log(req);
+    console.log('hello!');
     res.send('hello world');
 });
+
+app.get('/home',(req, res) => {
+    res.send('hello world');
+})
+
+app.get('/user/:id', (req, res) => {
+    console.log(req.params.id);
+    res.send('user id:' + req.params.id)
+})
+
+app.get('/home/*',(req, res) => {
+    res.send('hello world123');
+})
+
+
+
 
 app.listen(3000,function(){
     console.log('start listen http://localhost:3000/');
